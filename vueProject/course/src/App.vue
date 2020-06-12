608926872defd4a9515a0ac80232fbc410334ba6
kaili <likaistevens@gmail.com>
2020-06-12 16:59:55 -0400

    2020.6.12

---
+++ vueProject/course/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  /*这里写的样式 会在所有页面采用*/
+  *{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  /*取消router-link的下划线  router-link最后都会转化成a标签*/
+  a{
+    text-decoration: none;
+  }
+</style>
